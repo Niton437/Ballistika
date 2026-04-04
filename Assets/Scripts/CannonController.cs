4242bb3567df210c1deea63ac1a0a354f5260e32
Niton437 <niton437@yandex.ru>
2026-04-04 12:12:29 +0300

    commit project

---
+++ Assets/Scripts/CannonController.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Cannon
+{
+    public class CannonController : MonoBehaviour
+    {
+        [SerializeField] private float movementSpeed = 1f;
+        [SerializeField] private float rotationSpeed = 5f;
+
+        private InputSystem inputActions;
+        private BallisticController ballisticController;
+
+        private void Awake()
+        {
+            inputActions = new InputSystem();
+            inputActions.Enable();
+
+            ballisticController = GetComponentInChildren<BallisticController>();
+            if (ballisticController == null)
+            {
+                Debug.LogError("BallisticController not found in children.");
+            }
+
+            inputActions.Control.Shoot.started += context => ballisticController.Fire();
+        }
+
+        private void OnDestroy()
+        {
+            if (inputActions != null)
+            {
+                inputActions.Control.Shoot.started -= context => ballisticController.Fire();
+                inputActions.Disable();
+                inputActions.Dispose();
+            }
+        }
+
+        private void Update()
+        {
+            ProcessMovement();
+            ProcessRotation();
+        }
+
+        private void ProcessMovement()
+        {
+            Vector2 moveInput = inputActions.Control.WASD.ReadValue<Vector2>();
+            Vector3 moveDirection = (transform.right * moveInput.x) + (transform.forward * moveInput.y);
+            transform.position += moveDirection * (movementSpeed * Time.deltaTime);
+        }
+
+        private void ProcessRotation()
+        {
+            float rotateInput = inputActions.Control.Rotate.ReadValue<float>();
+            transform.Rotate(Vector3.up, rotateInput * rotationSpeed * Time.deltaTime);
+        }
+    }
+}
---
+++ Assets/Scripts/TargetSpawner.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+
+namespace Targets
+{
+    public class TargetSpawner : MonoBehaviour
+    {
+        [SerializeField] private GameObject targetPrefab;
+
+        [Header("Параметры области спавна")]
+        [SerializeField] private Vector2 areaSize = new Vector2(10f, 10f);
+        [SerializeField] private float depthOffset = 0.5f;
+        [SerializeField] private float spawnInterval;
+
+        [Header("Параметры мишеней")]
+        [SerializeField] private Vector3 startVelocity = new Vector3(5f, 0, 0);
+        [SerializeField, Range(0.01f, 100f)] private float minMass;
+        [SerializeField, Range(0.01f, 100f)] private float maxMass;
+        [SerializeField, Range(0.01f, 2f)] private float minRadius;
+        [SerializeField, Range(0.01f, 2f)] private float maxRadius;
+
+        private void Awake()
+        {
+            StartCoroutine(Spawn());
+        }
+
+        private IEnumerator Spawn()
+        {
+            while (true)
+            {
+                float randX = UnityEngine.Random.Range(-areaSize.x * 0.5f, areaSize.x * 0.5f);
+                float randY = UnityEngine.Random.Range(-areaSize.y * 0.5f, areaSize.y * 0.5f);
+
+                Vector3 spawnPosLocal = new Vector3(randX, randY, depthOffset);
+                Vector3 spawnPosWorld = transform.TransformPoint(spawnPosLocal);
+
+                GameObject newTarget = Instantiate(targetPrefab, spawnPosWorld, Quaternion.identity);
+
+                float targetRadius = UnityEngine.Random.Range(minRadius, maxRadius);
+                float targetMass = UnityEngine.Random.Range(minMass, maxMass);
+
+                newTarget.transform.localScale = new Vector3(0.1f, targetRadius, targetRadius);
+
+                Rigidbody targetRb = newTarget.GetComponent<Rigidbody>();
+                if (targetRb != null)
+                {
+                    targetRb.mass = targetMass;
+                    targetRb.useGravity = false;
+                    targetRb.linearVelocity = startVelocity;
+                }
+
+                Destroy(newTarget, 15f);
+                yield return new WaitForSeconds(spawnInterval);
+            }
+        }
+
+        private void OnDrawGizmosSelected()
+        {
+            Gizmos.color = Color.green;
+
+            Gizmos.matrix = transform.localToWorldMatrix;
+
+            Vector3 cubeSize = new Vector3(areaSize.x, areaSize.y, 0.01f);
+            Vector3 cubeCenter = new Vector3(0, 0, depthOffset);
+            Gizmos.DrawWireCube(cubeCenter, cubeSize);
+
+            Gizmos.matrix = Matrix4x4.identity;
+        }
+    }
+}
